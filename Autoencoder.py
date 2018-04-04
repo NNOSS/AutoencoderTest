115c2b4b8fb171ce988573bd231412aebbe2dec5
William Derksen <wderksen98@gmail.com>
2018-04-04 12:20:07 -0400

    finally got encoder testing working

--- Autoencoder.py
+++ Autoencoder.py
@@ -125,7 +125,7 @@ def conv_enc_net(x, is_train=False, reuse=False):
         hid3 = DenseLayer(flat3, encoding_size, act = tf.nn.relu, name = 'hidden_encode')
     return hid3
 
-def conv_dec_net(encoded, nx, ny, n_out=1):
+def conv_dec_net(encoded, nx, ny, reuse = None, n_out=1):
     with tf.variable_scope("u_net", reuse = None):
         hid3 = DenseLayer(encoded, 784, act = tf.nn.relu, name = 'hidden_decode')
         shape3 = ReshapeLayer(hid3, (-1, 7, 7, 16), name = 'unflatten')
@@ -158,10 +158,36 @@ def variable_summaries(var):
 # Construct model
 if(convolutional):
   _, nx, ny, nz = X.get_shape().as_list()
-  encoding = conv_enc_net(X, reuse = False)
-  enc_outs = encoding.outputs
+
+  with tf.variable_scope("u_net", reuse=None):
+        inputs = InputLayer(X, name='encoder_inputs')
+        conv1 = Conv2d(inputs, 4, (3, 3), act=tf.nn.relu, name='conv1_1')
+        pool1 = MaxPool2d(conv1, (2, 2), name='pool1')
+
+        conv2 = Conv2d(pool1, 8, (3, 3), act=tf.nn.relu, name='conv2_1')
+        pool2 = MaxPool2d(conv2, (2, 2), name='pool2')
+
+        conv3 = Conv2d(pool2, 16, (3, 3), act=tf.nn.relu, name='conv3_1')
+
+        flat3 = FlattenLayer(conv3, name = 'flatten')
+        enc3 = DenseLayer(flat3, encoding_size, act = tf.nn.relu, name = 'hidden_encode')
+        dec3 = DenseLayer(enc3, 784, act = tf.nn.relu, name = 'hidden_decode')
+        shape3 = ReshapeLayer(dec3, (-1, 7, 7, 16), name = 'unflatten')
+
+        conv3 = Conv2d(shape3, 8, (3, 3), act=tf.nn.relu, name='conv3_2')
+        up2 = DeConv2d(conv3, 8, (3, 3), (nx/2, ny/2), (2, 2), name='deconv2')
+        #print(tf.shape(up2.outputs))
+        #up2 = ConcatLayer([up2, conv2] , 3, name='concat2')
+        conv2 = Conv2d(up2, 8, (3, 3), act=tf.nn.relu, name='uconv2_1')
+        up1 = DeConv2d(conv2, 4, (3, 3), (nx/1, ny/1), (2, 2), name='deconv1')
+        #up1 = ConcatLayer([up1, conv1] , 3, name='concat1')
+        conv1 = Conv2d(up1, 4, (3, 3), act=tf.nn.relu, name='uconv1_1')
+        conv1 = Conv2d(conv1, 1, (1, 1), act=tf.nn.sigmoid, name='uconv1')
+
+  #encoding = conv_enc_net(X, reuse = False)
+  enc_outs = enc3.outputs
   variable_summaries(enc_outs)
-  f = conv_dec_net(encoding, nx, ny).outputs
+  f = conv1.outputs#conv_dec_net(encoding, nx, ny, reuse = None).outputs
 
   #Output Summary
   f_255 = tf.image.convert_image_dtype (f, dtype=tf.uint8)
@@ -214,7 +240,7 @@ for epoch in range(training_epochs):
         batch_x, batch_y = mnist.train.next_batch(batch_size)
         # Run optimization op (backprop) and cost op (to get loss value)
         if (step % encoding_print_step == 0):
-          dec_summy = sess.run(dec_summary, feed_dict={encoding.outputs: 30*np.eye(encoding_size)}) # Y is same as X
+          dec_summy = sess.run(dec_summary, feed_dict={enc3.outputs: 30*np.eye(encoding_size)}) # Y is same as X
           sum_writer.add_summary(dec_summy, step);
         
         if (step % summy_write_step == 0):
--- EncoderTest.py
+++ EncoderTest.py
@@ -5,19 +5,23 @@ import tensorlayer as tl
 import numpy as np
 from tensorlayer.layers import *
 
-merged = tf.summary.merge_all()
+#merged = tf.summary.merge_all()
 
 with tf.Session() as sess:
-	
 
-    saver = tf.train.import_meta_graph('./Model/Mnist-Encoding.meta')
-    saver.restore(sess,tf.train.latest_checkpoint('./Model/'))
+    saver = tf.train.import_meta_graph('./Model2/Mnist-Encoding.meta')
+    saver.restore(sess,tf.train.latest_checkpoint('./Model2/'))
 
     graph = tf.get_default_graph()
-    namelist = [n.name for n in tf.get_default_graph().as_graph_def().node]
-    print(namelist)
-    decoder_in = graph.get_tensor_by_name("u_net/hidden_encode:0")
-    #final = graph.get_tensor_by_name("u_net/uconv1:0")
+    #namelist = [n.name for n in tf.get_default_graph().as_graph_def().node]
+    #print(namelist)
+    decoder_in = graph.get_tensor_by_name("u_net/hidden_encode/Relu:0")
+    final = graph.get_tensor_by_name("decoding:0")
+    step = 0
+    sum_writer = tf.summary.FileWriter("logs_enctest", sess.graph)
+
+    init = tf.global_variables_initializer()
+    sess.run(init)
 
     while(True):
 		in_ind = raw_input("pick a valid integer: ")
@@ -25,7 +29,10 @@ with tf.Session() as sess:
 			in_ind = int(in_ind)
 		except:
 			continue
-		test = np.zeros(12)
+		test = np.zeros(25)
 		test[in_ind] = 1;
-		feed_dict = {encoding: test}
-		print(sess.run(final,feed_dict))
+		test = np.expand_dims(test, axis=0)
+		feed_dict = {decoder_in: test}
+		out = sess.run(final,feed_dict)
+		sum_writer.add_summary(out, step)
+		step += 1;
